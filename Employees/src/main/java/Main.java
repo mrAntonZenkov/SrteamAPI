13a58eaafef29e3109fee01408e02c8cf36182d9
mrAntonZenkov <90611003+mrAntonZenkov@users.noreply.github.com>
2023-07-18 20:43:54 +0300

    Add files via upload

---
+++ Employees/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    private static final String STAFF_TXT = "data/staff.txt";
+
+    public static void main(String[] args) {
+        List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
+        System.out.println(employeeMaxSalary);
+    }
+
+    public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
+        return staff.stream().filter(employee -> employee.getWorkStart()
+                        .toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .getYear() == year)
+                .max(Comparator.comparing(Employee::getSalary)).get();
+
+    }
+}
